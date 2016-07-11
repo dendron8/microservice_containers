cad7bed93ba53bd3e4a4263eba35f31a10c331e7
phil helm <phelm@metlife.com>
2016-07-11 13:36:00 -0400

    change from localhost to loopback

--- sample_microservice/src/main/java/sample/microservice/SampleController.java
+++ sample_microservice/src/main/java/sample/microservice/SampleController.java
@@ -13,7 +13,7 @@ public class SampleController {
 		Cluster cluster;
 		Session session;
 		
-		cluster = Cluster.builder().addContactPoint("localhost").build();
+		cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
 		//lets see what happens when we commit
 		session = cluster.connect("mykeyspace");
 		
