721863223606dc559a37385f696e554f20a45a5e
phil helm <phelm@metlife.com>
2016-08-04 15:45:52 -0400

    enable hystrix client stream

--- sample_microservice/src/main/java/sample/microservice/SampleController.java
+++ sample_microservice/src/main/java/sample/microservice/SampleController.java
@@ -2,24 +2,31 @@ package sample.microservice;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.context.annotation.Configuration;
 
 import com.datastax.driver.core.*;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @EnableEurekaClient
 @Configuration
 @EnableAutoConfiguration
+@EnableCircuitBreaker
+
 public class SampleController {
 	
-	@Value("${property1}")
+	//@Value("${property1}")
 	  String prop;
 	
 	@RequestMapping("/")
+	@HystrixCommand
 	public String helloWorld() {
 		
 		Cluster cluster;
